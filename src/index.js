09799bd61d04b1a5a9a5fc487256c2ec8fb891a3
Emir Mert ÇAMLI <51798175+EmirMC@users.noreply.github.com>
2023-03-19 19:33:29 +0300

    Dynamic env folder and lambda function name

--- src/index.js
+++ src/index.js
@@ -8,27 +8,41 @@ const prompt = require("prompt-sync")({ sigint: true });
 const deploymentDir = process.argv[2];
 const rel = (relPath) => path.resolve(deploymentDir, relPath);
 
-require("dotenv").config({ path: rel(".env") });
-
 const parse = require("./helpers/parserEnv");
 const exec = util.promisify(child_process.exec);
 
 (async () => {
   console.time("Running time");
-  let STAGE = "main";
+
+  // Get stage name
+  let STAGE = "production";
   const ANSWER_STAGE = prompt(`Stage name [${STAGE}]: `);
   if (ANSWER_STAGE?.trim()?.length > 0) STAGE = ANSWER_STAGE;
-  let LAMBDA_FUNCTION_NAME = deploymentDir.split("/").pop() + "-" + STAGE + "-hitook";
+
+  // Get lambda function name
+  const splited_dir = deploymentDir.split("/");
+  let LAMBDA_FUNCTION_NAME =
+    splited_dir[splited_dir.length - 2] + "-" + splited_dir[splited_dir.length - 1] + "-" + STAGE;
   const ANSWER_LAMBDA_FUNCTION_NAME = prompt(`Lambda function name [${LAMBDA_FUNCTION_NAME}]: `);
   if (ANSWER_LAMBDA_FUNCTION_NAME?.trim()?.length > 0) LAMBDA_FUNCTION_NAME = ANSWER_LAMBDA_FUNCTION_NAME;
-  const variables = await parse(rel(".env"));
+
+  // Get environment folder name
+  let ENVIRONMENT_FOLDER = `.env.${STAGE}`;
+  const ANSWER_ENVIRONMENT_FOLDER = prompt(`Lambda function name [${ENVIRONMENT_FOLDER}]: `);
+  if (ANSWER_ENVIRONMENT_FOLDER?.trim()?.length > 0) ENVIRONMENT_FOLDER = ANSWER_ENVIRONMENT_FOLDER;
+
+  // Get environment variables
+  const variables = await parse(rel(ENVIRONMENT_FOLDER));
+
   console.log(`Pushing to ${LAMBDA_FUNCTION_NAME} AWS Lambda...`);
+  // Push to AWS Lambda
   await exec(
     `aws lambda update-function-configuration \
     --function-name ${LAMBDA_FUNCTION_NAME} \
     --environment "Variables={${variables}}"`,
     { cwd: deploymentDir }
   );
+
   console.log(`Pushed to ${LAMBDA_FUNCTION_NAME} AWS Lambda.`);
   console.log("Environment variables: \n", variables);
   console.timeEnd("Running time");
